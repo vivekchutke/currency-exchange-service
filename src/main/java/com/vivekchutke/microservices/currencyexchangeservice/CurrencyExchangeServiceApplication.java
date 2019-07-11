ed1eebed0904fbdae0cf2e442b457d98b86e07b9
vivekchutke <vivekchutke@gmail.com>
2019-07-10 19:01:32 -0700

    CUPS and other ENV var changes

--- src/main/java/com/vivekchutke/microservices/currencyexchangeservice/CurrencyExchangeServiceApplication.java
+++ src/main/java/com/vivekchutke/microservices/currencyexchangeservice/CurrencyExchangeServiceApplication.java
@@ -3,12 +3,14 @@ package com.vivekchutke.microservices.currencyexchangeservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 
 import brave.sampler.Sampler;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableEurekaClient
 public class CurrencyExchangeServiceApplication {
 
 	public static void main(String[] args) {
